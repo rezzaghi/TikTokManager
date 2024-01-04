9f038d50cc77da08bc0f46a7a976284cf92a084e
rezzaghi <lbrezzaghi@gmail.com>
2024-01-04 19:34:43 -0300

    simple notification

--- app/src/main/java/com/tiktokmanager/data/TrackTimeService.kt
+++ app/src/main/java/com/tiktokmanager/data/TrackTimeService.kt
@@ -1,5 +1,6 @@
 package com.tiktokmanager.data
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -7,12 +8,16 @@ import android.app.usage.UsageEvents
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.pm.ServiceInfo.FOREGROUND_SERVICE_TYPE_HEALTH
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.tiktokmanager.R
 
 
 class TrackTimeService : Service() {
@@ -28,17 +33,27 @@ class TrackTimeService : Service() {
     override fun onCreate() {
         super.onCreate()
         createNotificationChannel()
+
         val notification = NotificationCompat.Builder(this, "manager")
-            .build()
-        startForeground(1, notification, FOREGROUND_SERVICE_TYPE_HEALTH)
+            .setContentTitle("service started")
+            .setContentText("tracking tiktok usage")
+            .setSmallIcon(androidx.core.R.drawable.notification_icon_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        startForeground(1, notification.build(), FOREGROUND_SERVICE_TYPE_HEALTH)
     }
 
+    var test = true
     private fun startPolling() {
         val handler = Handler(Looper.getMainLooper())
         val tikTokCheckRunnable = object : Runnable {
             override fun run() {
                 if (isTikTokRunning()) {
                     println("way")
+                    if (test) {
+                        test = false
+                        showNotification()
+                    }
+
                 } else {
                     println("nay")
                 }
@@ -49,6 +64,24 @@ class TrackTimeService : Service() {
             }
         }
         handler.post(tikTokCheckRunnable)
+    }
+
+    private fun showNotification() {
+        val notification = NotificationCompat.Builder(this, "manager")
+            .setContentTitle("hello")
+            .setContentText("description?")
+            .setSmallIcon(androidx.core.R.drawable.notification_icon_background)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //TODO: ask user for permission
+            }
+            notify(1, notification.build())
+        }
     }
 
     private fun isTikTokRunning(): Boolean {
@@ -84,7 +117,7 @@ class TrackTimeService : Service() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "manager"
             val descriptionText = "sample"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val importance = NotificationManager.IMPORTANCE_HIGH
             val channel = NotificationChannel("manager", name, importance).apply {
                 description = descriptionText
             }
